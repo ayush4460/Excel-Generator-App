740d60d7c1aa29043a2bfba5ddb4328ae524a46f
Ayush Zala <ayushgzala@gmail.com>
2023-07-13 22:37:47 +0530

    First Commit

--- server/index.js
+++ server/index.js
@@ -1,29 +1,46 @@
 import express from 'express';
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import cors from "cors";
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import { ExcelModel } from './model/model.js';
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = 8000;
 
+app.listen(PORT, () => {
+  console.log(`Server successfully connected at http://localhost:${PORT}`);
+});
 
-
-app.listen(PORT,()=>{
-    console.log(`Server successfully connected at http://localhost:${PORT}`)
-})
+app.post('/api/create', async (req, res) => {
+  try {
+    const create = await ExcelModel.create(req.body);
+    res.status(200).json({
+      message: 'Create operation successfully performed',
+      success: true,
+      service: create
+    });
+  } catch (error) {
+    console.error('Error in create API', error.message);
+    res.status(500).json({
+      message: 'Failed to create',
+      success: false
+    });
+  }
+});
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017", { useNewUrlParser: true })
+  .connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true })
   .then(() => {
-    console.log("DB connected");
-    app.get("/", (req, res) => {
-      res.send("Hello World!");
+    console.log('DB connected');
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
     });
   })
   .catch((err) => {
-    databasestatus = err;
-    console.log("DB Error => ", err);
+    console.log('DB Error => ', err);
   });
--- server/model/model.js
+++ server/model/model.js
@@ -1,27 +1,26 @@
 import mongoose from 'mongoose';
 
 const ExcelSchema = new mongoose.Schema({
-    Name:{
-        type: String,
-        required: true
-    },
-    Email:{
-        type: String,
-        required: true
-    },
-    Phone:{
-        type: Number,
-        required: true
-    },
-    Gender:{
-        type: Boolean,
-        required: true
-    },
-    Message:{
-        type: Number,
-        required: true
-    },
-},{timestamps: true}
-)
+  Name: {
+    type: String,
+    required: true
+  },
+  Email: {
+    type: String,
+    required: true
+  },
+  Phone: {
+    type: Number,
+    required: true
+  },
+  Gender: {
+    type: String,
+    required: true
+  },
+  Message: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
 
-module.exports = mongoose.model("ExcelSchema",ExcelSchema)
+export const ExcelModel = mongoose.model('ExcelSchema', ExcelSchema);
